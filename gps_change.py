75333e92de921f75c7487ca7abdf0caf9792df2d
muzy06038 <sub06038@khu.ac.kr>
2021-09-28 19:35:23 +0900

    road detect

---
+++ gps_change.py
@@ -0,0 +1,5 @@
+import aigo_destination_route
+
+
+aigo_destination_route.get_route("경희대학교 중앙도서관")
+
--- live.py
+++ live.py
@@ -8,26 +8,73 @@ from modules.models.sync_batchnorm.replicate import patch_replication_callback
 from PIL import Image
 import os.path as osp
 
-MODEL_PATH = "./run/surface/deeplab/model_iou_77.pth.tar"
+import aigo_destination_route
+import threading
+#from ublox_gps import UbloxGps
+import serial
+import math
+
+
+#aigo_destination_route.get_route("경희대학교 중앙도서관")
+
+
+
+
+def gpsTracking():
+    port = serial.Serial('/dev/ttyACM0', baudrate=460800, timeout=1)
+    gps = UbloxGps(port)
+    point_check = 0
+    with open('points.txt', 'r') as points:
+        currentline = points.readline()
+        splitline = currentline.split(',')
+        while True:
+            try: 
+                coords = gps.geo_coords()
+                print(coords.lon, coords.lat)
+                current_location_lon = coords.lon
+                current_location_lat = coords.lat
+            except (ValueError, IOError) as err:
+                print(err) 
+
+            if(point_check == 1):
+                currentline = points.readline()
+                if(currentline == ''):
+                    break
+                splitline = currentline.split(',')
+                point_check = 0
+
+            point_lon = float(splitline[0])
+            point_lat = float(splitline[1])
+            route_info = splitline[2]
+
+            PTCdistance = math.sqrt((point_lon-current_location_lon)**2 + (point_lat-current_location_lat)**2)
+            if (PTCdistance<=0.00001):
+                print(route_info)
+                point_check = 1
+            
+    port.close()
+
+# t1 = threading.Thread(target=gpsTracking)
+# t1.start()
+
+
+
+MODEL_PATH = "./run/surface/deeplab/model_best.pth.tar"
 ORIGINAL_HEIGHT = 480
 ORIGINAL_WIDTH = 640
 MODEL_HEIGHT = 480 #512
 MODEL_WIDTH =  640 #1024
-NUM_CLASSES = 7  # including background
+NUM_CLASSES = 4  # including background
 CUDA = True if torch.cuda.is_available() else False
 
 MODE = 'jpg'  # 'mp4' or 'jpg'
-
-OVERLAPPING = True  # whether to mix segmentation map and original image
+OVERLAPPING = False  # whether to mix segmentation map and original image
 
 CUSTOM_COLOR_MAP = [
     [0, 0, 0],  # background
-    [255, 128, 0],  # bike_lane
-    [255, 0, 0],  # caution_zone
     [255, 0, 255],  # crosswalk
-    [255, 255, 0],  # guide_block
-    [0, 0, 255],  # roadway
-    [0, 255, 0],
+    [255, 0, 0],  # roadway
+    [0, 255, 0],  # sidewalk
 ]  # To ignore unused classes while predicting
 CUSTOM_N_CLASSES = len(CUSTOM_COLOR_MAP)
 
@@ -80,6 +127,11 @@ model_wrapper = ModelWrapper()
 
 capture = cv2.VideoCapture(0)
 
+
+idx = np.array([320])
+
+road_way_count = 0 
+
 while True:
         ret, frame = capture.read()
 
@@ -95,8 +147,20 @@ while True:
         else:
             result = segmap
 
-        cv2.imshow('image', result)
-            
+        # boundary = np.where((result[:,:,0] == 255)&(result[:,:,1] == 0)&(result[:,:,2] == 0))
+        # print(boundary)
+        # print(result.shape)
+
+        detect_array = result[200:265,320:330]
+        boolean_indexing = (detect_array[:,:,0] == 255)&(detect_array[:,:,1] == 0)&(detect_array[:,:,2] == 0)
+        boolean_set = np.unique(boolean_indexing)
+        if(boolean_set.size == 1 & boolean_set[0] == True):
+            road_way_count+=1
+
+        print(road_way_count)
+
+        cv2.imshow('image', result[200:265,320:330])
+        cv2.imshow('image2', result)
         key = cv2.waitKey(1)
         if key == 27:
             break
